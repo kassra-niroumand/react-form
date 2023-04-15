78df671048257de02c4b939a3c1878c8a00b8004
kasra <kasra.niroumand@gmail.com>
2023-04-15 13:00:33 +0330

    controlled form

--- src/component/Form.tsx
+++ src/component/Form.tsx
@@ -1,30 +1,26 @@
-import React, { FormEvent, useRef } from 'react'
+import React, { FormEvent, useRef, useState } from 'react'
 
 export default function Form() {
 
-    const nameRef = useRef<HTMLInputElement>(null)
-    const ageRef = useRef<HTMLInputElement>(null)
+    const [person,setPerson] = useState({
+        name:'',
+        age:0
+    });
 
     function handleSubmit(event:FormEvent) {
         event.preventDefault()
-        if (nameRef.current != null) {
-            console.log(nameRef.current.value);            
-        } 
-        if (ageRef.current != null) {
-            console.log(parseInt(ageRef.current.value));
-            
-        }
+        console.log(person)
     }
 
   return (
     <form onSubmit={handleSubmit}>
         <div className="mb-3">
             <label htmlFor="name">name</label>
-            <input ref={nameRef} type="text" name="name" id="name" />
+            <input value={person.name} onChange={e=>setPerson({...person,name:e.target.value})} type="text" name="name" id="name" />
         </div>
         <div className="mb-3">
             <label htmlFor="age" className='form-label'>Age</label>
-            <input ref={ageRef} type="text" name="age" id="age" />
+            <input value={person.age} onChange={e=>setPerson({...person,age:+e.target.value})} type="text" name="age" id="age" />
         </div>
         <button type="submit">submit</button>
     </form>
